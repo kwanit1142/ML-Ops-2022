a717b89f1bc908ac6bc9fc61a8e6e4ddd56d9a3e
kwanit1142 <gupta.45@iitj.ac.in>
2022-11-20 21:37:27 +0530

    End_Semester

--- Day-2/svm_return.py
+++ Day-2/svm_return.py
@@ -1,26 +1,15 @@
 import numpy as np
 from sklearn import datasets,svm
 from sklearn.model_selection import train_test_split
-from skimage import transform
 from joblib import dump, load
-
-def new_data(data,size):
-	new_features = np.array(list(map(lambda img: transform.resize(img.reshape(8,8),(size,size),mode='constant',preserve_range=True).ravel(),data)))
-	return new_features
 
 digits = datasets.load_digits()
 user_split = 0.1
 n_samples = len(digits.images)
-user_size=8
-data = new_data(digits.data,user_size)
-GAMMA = 0.001
-C = 1
-hyper_params = {'gamma':GAMMA, 'C':C}
+data = digits.data
 clf = svm.SVC()
-clf.set_params(**hyper_params)
-X_train, X, Y_train, Y = train_test_split(data, digits.target, test_size=user_split, shuffle=False)
+X_train, X_rest, Y_train, Y_rest = train_test_split(data, digits.target, test_size=user_split, shuffle=False, random_state=10)
+print(type(X_train))
+X_val, X_test, Y_val, Y_test = train_test_split(X_rest, Y_rest, test_size=0.5, shuffle=False, random_state=10)
 clf.fit(X_train, Y_train)
-dump(clf,'best_svm_model.joblib')
-test = load('best_svm_model.joblib')
-print(test.predict(X))
-
+print(clf.predict(X_test))
--- Day-2/test_sample.py
+++ Day-2/test_sample.py
@@ -2,13 +2,15 @@ import numpy as np
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 
+X_v, X_t, Y_v, Y_t = 0,0,0,0
 digits = datasets.load_digits()
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples,-1))
-
-X_train ,X_test, y_train, y_test = train_test_split(data, digits.target,test_size=0.5,shuffle=False)
+user_random  = 10
+X_train ,X_rest, Y_train, Y_rest = train_test_split(data, digits.target,test_size=0.5,shuffle=True, random_state=user_random)
+X_val, X_test, Y_val, Y_test = train_test_split(X_rest, Y_rest, test_size=0.5, shuffle=True, random_state=user_random)
 clf = svm.SVC(gamma=0.001)
-clf.fit(X_train, y_train)
+clf.fit(X_train, Y_train)
 predictions = clf.predict(X_test)
 
 def test_biased():
@@ -22,3 +24,17 @@ def test_biased():
 def test_union():
 	class_set = {pred for pred in predictions}
 	assert len(class_set) == 10
+
+def test_same_seed():
+	count=0
+	X_v, X_t, Y_v, Y_t = train_test_split(X_rest, Y_rest, test_size=0.5, shuffle=True, random_state=10)
+	if np.array_equal(X_v, X_val) == np.array_equal(X_t, X_test) == np.array_equal(Y_v, Y_val) == np.array_equal(Y_t, Y_test) == True:
+		count+=1
+	assert count==1
+
+def test_different_seed():
+	count=0
+	X_v, X_t, Y_v, Y_t = train_test_split(X_rest, Y_rest, test_size=0.5, shuffle=True, random_state=user_random-1)
+	if np.array_equal(X_v,X_val) == np.array_equal(X_t,X_test) == np.array_equal(Y_v,Y_val) == np.array_equal(Y_t,Y_test) == False:
+		count+=1
+	assert count==1
